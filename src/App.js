e6ac36bdbfe9507be8cecf276a9db207537fd4f7
KimHanseul <lkj2822@gmail.com>
2025-03-27 04:55:35 +0900

    20250327-khs-4

--- src/App.js
+++ src/App.js
@@ -9,12 +9,12 @@ function App() {
   return (
     <div className="App">
       <BrowserRouter basename={process.env.PUBLIC_URL}>
-      <Header/>
-      <Routes>
-            <Route path="/" element={<Info />} />
-            <Route path="/album" element={<Album />} />
-            <Route path="/calendar" element={<Calendar />} />
-          </Routes>
+        <Header />
+        <Routes>
+          <Route path="/" element={<Info />} />
+          <Route path="/album" element={<Album />} />
+          <Route path="/calendar" element={<Calendar />} />
+        </Routes>
 
       </BrowserRouter>
     </div>
