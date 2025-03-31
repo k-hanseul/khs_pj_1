be3c122bd7e2eca085ae5ea8b5f2008baf76a730
KimHanseul <lkj2822@gmail.com>
2025-04-01 05:08:17 +0900

    20250401-khs-1

--- src/components/album/Album.jsx
+++ src/components/album/Album.jsx
@@ -13,11 +13,10 @@ function Album() {
     setWidth(window.innerWidth);
   }, 1000);
 
-   // const [totalPhotos, setTotalPhotos] = useState(getPhotoList);
   const [totalPhotos, setTotalPhotos] = useState([]);
   const [currPage, setPage] = useState(1);
   const [pagePhoto, setPagePhoto] = useState([]);
-  const pagePhotoCnt = 8;
+  const pagePhotoCnt = isMobile ? 4 : 8;
 
   function getPageData() {
     var data = totalPhotos.slice((currPage - 1) * pagePhotoCnt, currPage * pagePhotoCnt);
@@ -73,72 +72,24 @@ function Album() {
     getPageData();
   }, [currPage]);
 
-  // 페이지 변경 시 노출 데이터 변경
   // useEffect(() => {
-  //   var items = document.getElementsByClassName("photo_img");
-  //   // console.log("#### items.length: " + items.length + " / items: " + JSON.stringify(items));
-  //   // console.log("#### pagePhoto.length: " + pagePhoto.length + " / pagePhoto: " + JSON.stringify(pagePhoto));
-  //   console.log("#### pagePhoto.length: " + totalPhotos_.length + " / pagePhoto: " + JSON.stringify(totalPhotos_));
-
-  //   // items.forEach(function(item) {
-  //   //   item.style.display = "none";
-  //   // });
-
-  //   var data = totalPhotos_.slice((currPage - 1) * pagePhotoCnt, currPage * pagePhotoCnt);
-
-  //   // var data = totalPhotos.slice((currPage - 1) * pagePhotoCnt, currPage * pagePhotoCnt);
-  //   setPagePhoto(data);
-  // }, [currPage]);
-
-  useEffect(() => {
-    console.log("#### pagePhoto: " + JSON.stringify(pagePhoto));
-  }, [pagePhoto]);
-
-  // useEffect(() => {
-  //   console.log("#### imagesPreloaded: " + imagesPreloaded);
-  //   if (imagesPreloaded) {
-  //     console.log("#### imagesPreloaded totalPhotos: " + JSON.stringify(totalPhotos) + " / " + totalPhotos.length);
-  //     console.log("#### 22 test: " + test);
-  //     getPageData();
-  //   }
-  // }, [imagesPreloaded]);
-
-
+  //   console.log("#### pagePhoto: " + JSON.stringify(pagePhoto));
+  // }, [pagePhoto]);
 
   function getPageData() {
     var data = totalPhotos.slice((currPage - 1) * pagePhotoCnt, currPage * pagePhotoCnt);
-    console.log("#### getPageData: " + JSON.stringify(data));
-
+    // console.log("#### getPageData: " + JSON.stringify(data));
     setPagePhoto(data);
   }
-
-
-
-
-
-
-
-
-
-
 
   return (
     <div className={style.album}>
       <div className={style.photo_section_t1}>
         <div className={style.photo_list}>
           {
-            // totalPhotos.map((p, i) => (
-            //   <div className={style.photo_item} key={i}>
-            //     <img className={style.photo_img} src={p} alt={"photo_" + i} />
-            //   </div>
-            // ))
             [...Array(pagePhotoCnt)].map((e, i) => (
               <div className={style.photo_item} key={i}>
-                <AnimatePresence initial={false}>
-
                 <img className={style.photo_img} style={{ display: pagePhoto.length > i ? "none" : "block", objectFit: "scale-down", opacity: "0.5" }} src={process.env.PUBLIC_URL + '/img/logo_cat_.png'} alt={"default_photo_" + i} />
-                </AnimatePresence>
-
                 <img className={style.photo_img} style={{ display: pagePhoto.length > i ? "block" : "none" }} src={pagePhoto[i]} alt={"photo_" + i} />
               </div>
             ))
@@ -147,8 +98,7 @@ function Album() {
         <div className={style.pagination}>
           <Pagination
             current={currPage}
-            // total={totalPhotos.length}
-            total={11}
+            total={totalPhotos.length}
             pageSize={pagePhotoCnt}
             showLessItems={true}
             onChange={(page) => setPage(page)}
@@ -157,50 +107,6 @@ function Album() {
       </div>
     </div>
   )
-
-  /*
-  return (
-    <div className={style.album}>
-      {
-        getPhotoList() > 0 ?
-          <div className={style.photo_section_t1}>
-            <div className={style.photo_list}>
-              {
-                // totalPhotos.map((p, i) => (
-                //   <div className={style.photo_item} key={i}>
-                //     <img className={style.photo_img} src={p} alt={"photo_" + i} />
-                //   </div>
-                // ))
-                [...Array(pagePhotoCnt)].map((e, i) => (
-                  <div className={style.photo_item} style={{ display: pagePhoto.length > i ? "block" : "block" }} key={i}>
-                    {
-                      pagePhoto.length > i &&
-                      <img className={style.photo_img} src={pagePhoto[i]} alt={"photo_" + i} />
-                      // <img className={style.photo_img} src={pagePhoto[i]} alt={"photo_" + i} loading={"lazy"} decoding={"async"}/>
-                    }
-
-                  </div>
-                ))
-              }
-            </div>
-            <div className={style.pagination}>
-              <Pagination
-                current={currPage}
-                total={totalPhotos.length}
-                pageSize={pagePhotoCnt}
-                showLessItems={true}
-                onChange={(page) => setPage(page)}
-              />
-            </div>
-          </div>
-          :
-          <div className={style.photo_section_t1}>
-            <h3>앨범 내 사진이 존재하지 않습니다. </h3>
-          </div>
-      }
-    </div>
-  )
-  */
 }
 
 export default Album
